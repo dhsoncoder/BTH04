67c51ae5949e785d2b6d02db25ecf038eeae59e0
Dang Hai Son <soncma2811@gmail.com>
2024-05-21 08:25:22 +0700

    version 1

--- app/src/main/java/com/example/qlsv/MainActivity.java
+++ app/src/main/java/com/example/qlsv/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.qlsv;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +18,119 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    EditText edtmalop,edttenlop,edtsiso;
+    Button btninsert,btnupdate,btndelete,btnquery;
+    // Khai bao listview
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        edtmalop = findViewById(R.id.edtmalop);
+        edtsiso = findViewById(R.id.edtsiso);
+        edttenlop = findViewById(R.id.edttenlop);
+        btndelete = findViewById(R.id.btndelete);
+        btninsert = findViewById(R.id.btninsert);
+        btnquery = findViewById(R.id.btnquery);
+        btnupdate = findViewById(R.id.btnupdate);
+        //Tao listview
+
+        lv=findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,mylist);
+        lv.setAdapter(myadapter);
+        mydatabase = openOrCreateDatabase("qlsinhvien.db",MODE_PRIVATE,null);
+
+        try {
+            String sql ="CREATE TABLE tbllop(malop TEXT primary key, tenlop TEXT, siso INTEGER)";
+            mydatabase.execSQL(sql);
+        }catch (Exception e){
+            Log.e("Error", "Table da ton tai");
+        }
+        reload();
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                String tenlop= edttenlop.getText().toString();
+                int siso= Integer.parseInt(edtsiso.getText().toString());
+                ContentValues myvalue = new ContentValues();
+                myvalue.put("malop",malop);
+                myvalue.put("tenlop",tenlop);
+                myvalue.put("siso",siso);
+                String msg="";
+                if(mydatabase.insert("tbllop",null,myvalue)==-1){
+                    msg="Lỗi ";
+
+                }else{
+                    msg = "Thêm thành công";
+                }
+                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+                reload();
+            }
+        });
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                int n = mydatabase.delete("tbllop","malop = ?",new String[]{malop});
+                String msg ="";
+                if(n==0){
+                    msg = "Không có bản ghi để xóa";
+
+                }else{
+                    msg= "Bản ghi "+n+"đã bị xóa";
+                }
+                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+                reload();
+            }
+        });
+        btnupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int siso = Integer.parseInt(edtsiso.getText().toString());
+                String malop = edtmalop.getText().toString();
+                ContentValues myvalue = new ContentValues();
+                myvalue.put("siso",siso);
+                int n= mydatabase.update("tbllop",myvalue,"malop =?",new String[]{malop});
+                String msg ="";
+                if(n==0){
+                    msg = "Không có bản ghi được cập nhật";
+
+                }else{
+                    msg= "Bản ghi "+n+"đã được cập nhật";
+                }
+                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+                reload();
+            }
+
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public  void reload(){
+        mylist.clear();
+        Cursor c = mydatabase.query("tbllop",null,null,null,null,null,null);
+        c.moveToNext();
+        String data = "";
+        while(c.isAfterLast()==false){
+            data = c.getString(0)+ " - "+ c.getString(1)+" - "+c.getString(2);
+            c.moveToNext();
+            mylist.add(data);
+
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
     }
 }
